257576abb1043c53ec8609209a32021f1b0fe24f
Whitney Young <wbyoung@fadingred.com>
2014-10-01 22:32:00 -0700

    Added callable tests.

--- test/util/callable_tests.js
+++ test/util/callable_tests.js
@@ -1,22 +1,70 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+chai.use(require('sinon-chai'));
+
 var callable = require('../../lib/util/callable');
 
 describe('callable', function() {
 
-  it('accepts a constructor function');
-  it('returns a class');
-  it('calls `call`');
-  it('uses callable object in `call`');
-  it('uses callable object in constructor');
-  it('passes arguments through to the constructor');
-  it('passes arguments through to `call`');
-  it('creates instances when called');
-  it('creates instances with a __proto__ set to the class prototype');
+  beforeEach(function() {
+    this.constructorSpy = sinon.spy();
+    this.callSpy = sinon.spy();
+    this.Callable = callable(this.constructorSpy);
+    this.Callable.prototype.call = this.callSpy;
+  });
 
-  describe('created instance', function() {
-    it('is a type of the class');
-    it('can call methods defined on the class');
+  it('returns a class', function() {
+    expect(this.Callable.prototype).to.exist;
+  });
+
+  describe('instance', function() {
+    beforeEach(function() {
+      this.instance = new this.Callable('creating', 'callable');
+    });
+    it('exists', function() {
+      expect(this.instance).to.exist;
+    });
+    it('calls the constructor', function() {
+      expect(this.constructorSpy).to.have.been.calledOnce;
+    });
+    it('uses callable object in constructor', function() {
+      expect(this.constructorSpy.firstCall.thisValue).to.equal(this.instance);
+    });
+    it('passes arguments through to the constructor', function() {
+      expect(this.constructorSpy).to.have.been.calledWithExactly('creating', 'callable');
+    });
+    it('has a __proto__ set to the class prototype', function() {
+      expect(this.instance.__proto__).to.equal(this.Callable.prototype);
+    });
+    it('is a type of the class', function() {
+      expect(this.instance).to.be.an.instanceOf(this.Callable);
+    });
+    it('does not call the `call` function', function() {
+      expect(this.callSpy).to.not.have.been.called;
+    })
+    it('can call methods defined on the class', function() {
+      var spy = sinon.spy();
+      this.Callable.prototype.testMethod = spy;
+      this.instance.testMethod();
+      expect(spy).to.have.been.called;
+    });
+
+    describe('when called', function() {
+      beforeEach(function() {
+        this.instance('calling', 'callable');
+      });
+      it('calls `call`', function() {
+        expect(this.callSpy).to.have.been.calledOnce;
+      });
+      it('uses callable object in `call`', function() {
+        expect(this.callSpy.firstCall.thisValue).to.equal(this.instance);
+      });
+      it('passes arguments through to `call`', function() {
+        expect(this.callSpy).to.have.been.calledWithExactly('calling', 'callable');
+      });
+    });
   });
 });
