154d84aa7b7cf8d47cecd5ba20b05440026fe12d
Whitney Young <wbyoung@fadingred.com>
2014-10-07 18:11:05 -0700

    Refined integration test ideas.

--- test/db/integration/pg_tests.js
+++ test/db/integration/pg_tests.js
@@ -8,16 +8,19 @@
 if (!/^(1|true)$/i.test(process.env.TEST_POSTGRES || '1')) { return; }
 
 var expect = require('chai').expect;
+var path = require('path');
 var Database = require('../../../lib/db/database');
 
+var connection = {
+  adapter: 'pg',
+  username: 'root',
+  password: '',
+  database: 'agave_test'
+};
+
 describe('PostgresQL', function() {
+  // TODO: should not connect to db until there's something to do (query)
   it('connects to the database', function(done) {
-    var connection = {
-      adapter: 'pg',
-      username: 'root',
-      password: '',
-      database: 'agave_test'
-    };
     var db = Database.create(connection);
     db.ready().then(function(db2) {
       expect(db).to.eql(db2);
@@ -26,4 +29,25 @@ describe('PostgresQL', function() {
     })
     .done(done, done);
   });
+
+  describe('with migrations applied', function() {
+    before(function(done) {
+      var db = this.db = Database.create(connection);
+      var migration = this.migration =
+        path.join(__dirname, '../../fixtures/migrations/blog');
+      db.schema.migrate(migration).then(function() { done(); }, done);
+    });
+
+    after(function(done) {
+      var db = this.db;
+      var migration = this.migration;
+      db.schema.rollback(migration).then(function() { done(); }, done);
+    });
+
+    // it('inserts data');
+    // it('selects data');
+    // it('updates data');
+    // it('drops tables');
+  });
+
 });
