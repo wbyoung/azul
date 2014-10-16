4ddb132cbd3a8138fdf8e471eeb6d7fa5437fddd
Whitney Young <wbyoung@fadingred.com>
2014-10-16 16:44:12 -0700

    Better joining.

--- test/db/schema_tests.js
+++ test/db/schema_tests.js
@@ -23,6 +23,28 @@ describe('Schema', function() {
       ));
     });
 
+    it('generates the proper sql with multiple columns', function() {
+      var query = schema.createTable('users', function(table) {
+        table.serial('id');
+        table.string('name');
+      });
+      expect(query.sql()).to.eql(Statement.create(
+        'create table users (id serial, name varchar(255))', []
+      ));
+    });
+
+
+    describe('types', function() {
+      it('handles serial', function() {
+        var query = schema.createTable('users', function(table) {
+          table.serial('id');
+        });
+        expect(query.sql()).to.eql(Statement.create(
+          'create table users (id serial)', []
+        ));
+      });
+    });
+
     it('is thenable', function() {
       var query = schema.createTable('users', function(table) {
         table.string('name');
