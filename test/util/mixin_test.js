03639efb170139ac877da11894d4325f90ca8134
Whitney Young <wbyoung@fadingred.com>
2014-10-22 18:51:52 -0700

    Made describe statement singular.

--- test/util/mixin_test.js
+++ test/util/mixin_test.js
@@ -6,7 +6,7 @@ var expect = chai.expect;
 var Class = require('../../lib/util/class');
 var Mixin = require('../../lib/util/mixin');
 
-describe('Mixins', function() {
+describe('Mixin', function() {
   it('looks like an object', function() {
     var properties = {
       first: 'first',
