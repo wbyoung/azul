37384ff4f1e1aeef4d9d8e924be03d5b920a8d85
Whitney Young <wbyoung@fadingred.com>
2014-10-30 18:37:19 -0700

    Added a few more update query tests & fixed a few things.

--- test/db/query/update_tests.js
+++ test/db/query/update_tests.js
@@ -7,6 +7,10 @@ var Database = require('../../../lib/db');
 var UpdateQuery = require('../../../lib/db/query/update');
 var MockAdapter = require('../../mocks/adapter');
 var Statement = require('../../../lib/db/grammar/statement');
+var LiteralString = require('../../../lib/db/condition/literal'),
+  l = LiteralString.l;
+var FieldString = require('../../../lib/db/condition/field'),
+  f = FieldString.f;
 
 var db;
 
@@ -27,14 +31,41 @@ describe('UpdateQuery', function() {
     ));
   });
 
+  it('updates multiple values', function() {
+    var query = db.update('users', { first: 'Whitney', last: 'Young' });
+    expect(query.sql()).to.eql(Statement.create(
+      'UPDATE "users" SET "first" = ?, "last" = ?', ['Whitney', 'Young']
+    ));
+  });
+
   it('updates data via #set', function() {
     expect(db.update('users').set({ name: 'Whitney' }).sql()).to.eql(Statement.create(
       'UPDATE "users" SET "name" = ?', ['Whitney']
     ));
   });
 
-  it('multiple calls to #set merge in new values');
-  it('works with literal statements');
+  it('multiple calls to #set merge in new values', function() {
+    var query = db.update('users')
+      .set({ first: 'Whitney', last: 'Young' })
+      .set({ first: 'Whit' });
+    expect(query.sql()).to.eql(Statement.create(
+      'UPDATE "users" SET "first" = ?, "last" = ?', ['Whit', 'Young']
+    ));
+  });
+
+  it('works with literal based statements', function() {
+    var query = db.update('users', { name: l('"first" + "last"') });
+    expect(query.sql()).to.eql(Statement.create(
+      'UPDATE "users" SET "name" = "first" + "last"', []
+    ));
+  });
+
+  it('works with field based statements', function() {
+    var query = db.update('users', { name: f('first') });
+    expect(query.sql()).to.eql(Statement.create(
+      'UPDATE "users" SET "name" = "first"', []
+    ));
+  });
 
   it('cannot be used without values to set', function() {
     var query = db.update('users');
