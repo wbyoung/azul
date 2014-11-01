df7f643b3ff9cc67960a4ff3620a673021f2a758
Whitney Young <wbyoung@fadingred.com>
2014-11-01 15:26:03 -0700

    A few translator tests.

--- test/db/grammar/translator_tests.js
+++ test/db/grammar/translator_tests.js
@@ -13,4 +13,28 @@ describe('Translator', function() {
     }).to.throw(/unhandled.*whatwhat/i);
   });
 
+  it('generates string with options', function() {
+    var translator = Translator.create();
+    expect(translator.type('string', { length: 20 })).to.eql('varchar(20)');
+  });
+
+  it('generates decimal with precision and scale', function() {
+    var translator = Translator.create();
+    var decimal = translator.type('decimal', { precision: 20, scale: 10 });
+    expect(decimal).to.eql('numeric(20, 10)');
+  });
+
+  it('generates decimal with precision', function() {
+    var translator = Translator.create();
+    var decimal = translator.type('decimal', { precision: 20 });
+    expect(decimal).to.eql('numeric(20)');
+  });
+
+  it('cannot generate decimal with just scale', function() {
+    var translator = Translator.create();
+    expect(function() {
+      translator.type('decimal', { scale: 2 });
+    }).to.throw(/specify precision.*scale/i);
+  });
+
 });
