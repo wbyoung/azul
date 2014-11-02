931598a91fa84465550a5e0dc6a957ea1948dae9
Whitney Young <wbyoung@fadingred.com>
2014-11-02 09:49:46 -0800

    Split shared behaviors into separate exports.

--- test/db/integration/shared_behaviors.js
+++ test/db/integration/shared_behaviors.js
@@ -62,112 +62,120 @@ module.exports.shouldRunMigrationsAndQueries = function() {
       })
       .done(done, done);
     });
+  });
+};
 
-    describe('types', function() {
+module.exports.shouldSupportStandardTypes = function() {
+  var db; before(function() { db = this.db; });
 
-      // shared behavior for type tests
-      var viaOptions = function(type, data, expected, options, equal, fn) {
-        var table = util.format('azul_type_%s', type);
-        var cast = function(result) {
-          return this.castDatabaseValue(type, result, options);
-        };
-        equal = equal || 'equal';
-        fn = fn || function() {};
-        return function(done) {
-          BluebirdPromise.bind(this)
-          .then(function() {
-            return db.schema.createTable(table, function(table) {
-              fn(table[type]('column', options));
-            });
-          })
-          .then(function() { return db.insert(table, { column: data }); })
-          .then(function() { return db.select(table); })
-          .get('rows')
-          .get('0')
-          .get('column')
-          .then(cast)
-          .then(function(result) {
-            expect(result).to[equal](expected);
-          })
-          .finally(function() { return db.schema.dropTable(table); })
-          .done(function() { done(); }, done);
-        };
-      };
-      var via = function(type, data, equal) {
-        return viaOptions(type, data, data, undefined, equal);
-      };
+  // shared behavior for type tests
+  var viaOptions = function(type, data, expected, options, equal, fn) {
+    var table = util.format('azul_type_%s', type);
+    var cast = function(result) {
+      return this.castDatabaseValue(type, result, options);
+    };
+    equal = equal || 'equal';
+    fn = fn || function() {};
+    return function(done) {
+      BluebirdPromise.bind(this)
+      .then(function() {
+        return db.schema.createTable(table, function(table) {
+          fn(table[type]('column', options));
+        });
+      })
+      .then(function() { return db.insert(table, { column: data }); })
+      .then(function() { return db.select(table); })
+      .get('rows')
+      .get('0')
+      .get('column')
+      .then(cast)
+      .then(function(result) {
+        expect(result).to[equal](expected);
+      })
+      .finally(function() { return db.schema.dropTable(table); })
+      .done(function() { done(); }, done);
+    };
+  };
+  var via = function(type, data, equal) {
+    return viaOptions(type, data, data, undefined, equal);
+  };
 
-      it('supports `auto`', via('auto', 1));
-      it('supports `increments`', via('increments', 1));
-      it('supports `serial`', via('serial', 1));
-      it('supports `integer`', via('integer', 1));
-      it('supports `integer64`', via('integer64', 1));
-      it('supports `string`', via('string', 'hello world'));
-      it('supports `text`', via('text', 'hello world'));
-      it('supports `binary`', via('binary', new Buffer('hello world'), 'eql'));
-      it('supports `bool`', via('bool', true));
-      it('supports `date`', via('date', new Date(2014, 10-1, 31), 'eql'));
-      it('supports `time`', via('time', '11:57:23'));
-      it('supports `dateTime`', via('dateTime', new Date(2014, 10-1, 31), 'eql'));
-      it('supports `float`', via('float', 3.14159));
-      it('supports `decimal`', via('decimal', 3.14159));
+  describe('types', function() {
 
-      it('supports `string` length', viaOptions('string', 'val', 'val', {
-        length: 3
-      }));
+    it('supports `auto`', via('auto', 1));
+    it('supports `increments`', via('increments', 1));
+    it('supports `serial`', via('serial', 1));
+    it('supports `integer`', via('integer', 1));
+    it('supports `integer64`', via('integer64', 1));
+    it('supports `string`', via('string', 'hello world'));
+    it('supports `text`', via('text', 'hello world'));
+    it('supports `binary`', via('binary', new Buffer('hello world'), 'eql'));
+    it('supports `bool`', via('bool', true));
+    it('supports `date`', via('date', new Date(2014, 10-1, 31), 'eql'));
+    it('supports `time`', via('time', '11:57:23'));
+    it('supports `dateTime`', via('dateTime', new Date(2014, 10-1, 31), 'eql'));
+    it('supports `float`', via('float', 3.14159));
+    it('supports `decimal`', via('decimal', 3.14159));
 
-      it('supports `decimal` precision', viaOptions('decimal', 3.14159, 3, {
-        precision: 3
-      }));
+    it('supports `string` length', viaOptions('string', 'val', 'val', {
+      length: 3
+    }));
 
-      it('supports `decimal` options', viaOptions('decimal', 3.14159, 3.14, {
-        precision: 4,
-        scale: 2
-      }));
+    it('supports `decimal` precision', viaOptions('decimal', 3.14159, 3, {
+      precision: 3
+    }));
 
-      it.skip('supports `pk`', viaOptions('string', 'key', 'key', {}, null,
-        function(col) { col.pk(); }));
+    it('supports `decimal` options', viaOptions('decimal', 3.14159, 3.14, {
+      precision: 4,
+      scale: 2
+    }));
 
-      it.skip('supports `primaryKey`', viaOptions('string', 'key', 'key', {}, null,
-        function(col) { col.primaryKey(); }));
+    it.skip('supports `pk`', viaOptions('string', 'key', 'key', {}, null,
+      function(col) { col.pk(); }));
 
-      it.skip('supports `indexed`', viaOptions('string', 'val', 'val', {}, null,
-        function(col) { col.indexed(); }));
+    it.skip('supports `primaryKey`', viaOptions('string', 'key', 'key', {}, null,
+      function(col) { col.primaryKey(); }));
 
-      it.skip('supports `default`', viaOptions('string', null, 'val', {}, null,
-        function(col) { col.default('val'); }));
+    it.skip('supports `indexed`', viaOptions('string', 'val', 'val', {}, null,
+      function(col) { col.indexed(); }));
 
-      it('supports `notNull`');
-      it('supports `unique`');
-    });
+    it.skip('supports `default`', viaOptions('string', null, 'val', {}, null,
+      function(col) { col.default('val'); }));
 
-    describe('conditions', function() {
-      it('supports `exact`');
-      it('supports `iexact`');
-      it('supports `in`');
-      it('supports `gt`');
-      it('supports `gte`');
-      it('supports `lt`');
-      it('supports `lte`');
-      it('supports `between`');
-      it('supports `isull`');
+    it('supports `notNull`');
+    it('supports `unique`');
+  });
+};
 
-      it('supports `contains`');
-      it('supports `icontains`');
-      it('supports `startsWith`');
-      it('supports `istartsWith`');
-      it('supports `endsWith`');
-      it('supports `iendsWith`');
-      it('supports `regex`');
-      it('supports `iregex`');
+module.exports.shouldSupportStandardConditions = function() {
+  var db; before(function() { db = this.db; });
 
-      it('supports `year`');
-      it('supports `month`');
-      it('supports `day`');
-      it('supports `weekday`');
-      it('supports `hour`');
-      it('supports `minute`');
-      it('supports `second`');
-    });
+  describe('conditions', function() {
+    it('supports `exact`');
+    it('supports `iexact`');
+    it('supports `in`');
+    it('supports `gt`');
+    it('supports `gte`');
+    it('supports `lt`');
+    it('supports `lte`');
+    it('supports `between`');
+    it('supports `isull`');
+
+    it('supports `contains`');
+    it('supports `icontains`');
+    it('supports `startsWith`');
+    it('supports `istartsWith`');
+    it('supports `endsWith`');
+    it('supports `iendsWith`');
+    it('supports `regex`');
+    it('supports `iregex`');
+
+    it('supports `year`');
+    it('supports `month`');
+    it('supports `day`');
+    it('supports `weekday`');
+    it('supports `hour`');
+    it('supports `minute`');
+    it('supports `second`');
   });
 };
