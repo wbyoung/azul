00fc2673931ed356191d17b89549756af639cf82
Whitney Young <wbyoung@fadingred.com>
2014-11-09 07:34:11 -0800

    Updated the migrate action output & added disconnect.

--- test/cli/cli_actions_tests.js
+++ test/cli/cli_actions_tests.js
@@ -5,42 +5,53 @@ var expect = chai.expect;
 var actions = require('../../lib/cli/actions');
 var cmd = require('./cli_helpers').cmd;
 var path = require('path');
-require('bluebird').longStackTraces();
 
-var adapter = require('../fakes/adapter').create({});
-var config = { adapter: adapter };
-var migrations = path.join(__dirname, '../fixtures/migrations/blog');
-var azulfile = {
-  development: config,
-  production: config,
-  staging: config,
-  test: config
-};
+var adapter,
+  config,
+  migrations,
+  azulfile;
 
-describe('CLI migrate', function() {
-  it('performs a schema migration', function(done) {
-    cmd({}, function() {
-      return actions.migrate(azulfile, { migrations: migrations });
-    })
-    .then(function(proc) {
-      expect(proc.exitStatus).to.eql(0);
-      expect(proc.exitCalled).to.eql(false);
-      expect(proc.stdout).to.match(/batch 1/i);
-      expect(proc.stdout).to.match(/applied 20141022202234_create_articles/i);
-      expect(proc.stdout).to.match(/applied 20141022202634_create_comments/i);
-    })
-    .done(done, done);
+describe('CLI', function() {
+  beforeEach(function() {
+    adapter = require('../fakes/adapter').create({});
+    config = { adapter: adapter };
+    migrations = path.join(__dirname, '../fixtures/migrations/blog');
+    azulfile = {
+      development: config,
+      production: config,
+      staging: config,
+      test: config
+    };
   });
 
-  it('fails schema migration when directory is missing', function(done) {
-    cmd({}, function() {
-      return actions.migrate(azulfile, { migrations: './missing-dir' });
-    })
-    .then(function(proc) {
-      expect(proc.exitStatus).to.eql(1);
-      expect(proc.exitCalled).to.eql(true);
-      expect(proc.stdout).to.match(/failed.*ENOENT.*missing-dir/i);
-    })
-    .done(done, done);
+  describe('migrate action', function() {
+    it('performs a schema migration', function(done) {
+      cmd({}, function() {
+        return actions.migrate(azulfile, { migrations: migrations });
+      })
+      .then(function(proc) {
+        expect(proc.exitStatus).to.eql(0);
+        expect(proc.exitCalled).to.eql(false);
+        expect(proc.stdout).to.match(/batch 1/i);
+        expect(proc.stdout).to.match(/20141022202234_create_articles/i);
+        expect(proc.stdout).to.match(/20141022202634_create_comments/i);
+      })
+      .done(done, done);
+    });
+
+    it('displays a up-to-date message when there is nothing to apply');
+
+    it('fails schema migration when directory is missing', function(done) {
+      cmd({}, function() {
+        return actions.migrate(azulfile, { migrations: './missing-dir' });
+      })
+      .then(function(proc) {
+        expect(proc.exitStatus).to.eql(1);
+        expect(proc.exitCalled).to.eql(true);
+        expect(proc.stdout).to.match(/failed.*ENOENT.*missing-dir/i);
+      })
+      .done(done, done);
+    });
   });
+
 });
