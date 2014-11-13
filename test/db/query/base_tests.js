fae69bc09f19550663971ae4a2d8c1b31d00ebbd
Whitney Young <wbyoung@fadingred.com>
2014-11-13 15:29:42 -0800

    A few tests for new aliases.

--- test/db/query/base_tests.js
+++ test/db/query/base_tests.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var chai = require('chai');
+var sinon = require('sinon'); chai.use(require('sinon-chai'));
 var expect = chai.expect;
 
 var BaseQuery = require('../../../lib/db/query/base');
@@ -16,6 +17,23 @@ describe('BaseQuery', function() {
   it('cannot generate sql', function() {
     expect(function() { query.sql(); })
       .throw(/BaseQuery.*cannot be used/i);
+  });
+
+  it('can be cloned', function() {
+    var clone = query.clone();
+    expect(clone).to.not.equal(query);
+    expect(clone._promise).to.not.equal(query._promise);
+  });
+
+  it('has a fetch method that aliases execute', function() {
+    sinon.stub(query, 'execute');
+    try {
+      query.fetch();
+      expect(query.execute).to.have.been.calledOnce;
+    }
+    finally {
+      query.execute.restore();
+    }
   });
 
   it('cannot be executed', function(done) {
