a4e0820d289c65bd152022c55e69f9928866362d
Whitney Young <wbyoung@fadingred.com>
2014-11-14 06:42:00 -0800

    Actually getting model objects back out of the database.

--- test/model_tests.js
+++ test/model_tests.js
@@ -8,26 +8,44 @@ var FakeAdapter = require('./fakes/adapter');
 var Statement = require('../lib/db/grammar/statement');
 
 var db,
+  adapter,
   Article,
   User;
 
 describe('Model', function() {
-  before(function() {
-    db = Database.create({ adapter: FakeAdapter.create({}) });
+  beforeEach(function() {
+    adapter = FakeAdapter.create({});
+    db = Database.create({ adapter: adapter });
     Article = db.Model.extend({});
     Article.reopenClass({ __name__: 'Article' });
 
     User = db.Model.extend({});
     User.reopenClass({ __name__: 'User' });
+
+    adapter.intercept(/select.*from "articles"/i, {
+      fields: ['id', 'title'],
+      rows: [{ id: 1, title: 'Existing Article' }]
+    });
   });
 
   it('knows its table', function() {
     expect(Article.tableName()).to.eql('articles');
   });
 
-  it('can get objects', function() {
-    expect(Article.objects.sql()).to.eql(Statement.create(
-      'SELECT * FROM "articles"', []
+  it('can get objects', function(done) {
+    Article.objects.fetch().then(function(articles) {
+      expect(adapter.executedSQL()).to.eql([
+        ['SELECT * FROM "articles"', []]
+      ]);
+      expect(articles).to.eql([Article.create({ id: 1, title: 'Existing Article' })]);
+    })
+    .done(done, done);
+  });
+
+  it.skip('can create objects', function() {
+    var article = Article.create({ title: 'Azul News' });
+    expect(article.save().sql()).to.eql(Statement.create(
+      'INSERT INTO "articles" ("title") VALUES (?)', ['Azul News']
     ));
   });
 });
