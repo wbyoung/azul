29f4b2e3971b35c672873f7e56f40e0f40d53765
Whitney Young <wbyoung@fadingred.com>
2014-12-02 11:17:40 -0800

    Added a few pending tests for inverse relationships.

--- test/relations/inverse_tests.js
+++ test/relations/inverse_tests.js
@@ -16,6 +16,50 @@ var db,
   article,
   articleObjects;
 
+var createdAuthor;
+var storedAuthor;
+var shared = {};
+
+shared.storeExistingAuthor = function(done) {
+  storedAuthor = User.fresh({ id: 5, username: 'jack' });
+  var promise = article.storeAuthor(storedAuthor);
+
+  // TODO: see comments below about expectations here
+  // expect(article.authorId).to.not.exist;
+  // expect(article.author).to.not.exist;
+
+  promise.then(function() {
+    // TODO: see comments below about expectations here
+    // expect(article.authorId).to.eql(storedAuthor.id);
+    // expect(article.author).to.equal(storedAuthor);
+  })
+  .done(done, done);
+};
+
+shared.storeUnsavedAuthor = function(done) {
+  storedAuthor = User.create({ username: 'jack' });
+  article.author = storedAuthor;
+  var promise = article.save();
+
+  // TODO: when do we want the following values to be set altered? before
+  // or after the save of the article?
+  //   - article.author
+  //   - article.authorId
+  //   - article.attrs.author_id
+  //   - storedAuthor.articles << article
+  // TODO: what happens when there is an error during the save? do the
+  // values get reverted?
+  // these are set after the promise is executed
+  // expect(article.authorId).to.not.exist;
+  // expect(article.author).to.not.exist;
+
+  promise.then(function() {
+    // TODO: see above
+    // expect(article.authorId).to.eql(storedAuthor.id);
+    // expect(article.author).to.equal(storedAuthor);
+  })
+  .done(done, done);
+};
 describe('Model.hasMany + Model.belongsTo', function() {
   beforeEach(function() {
     adapter = FakeAdapter.create({});
@@ -54,7 +98,6 @@ describe('Model.hasMany + Model.belongsTo', function() {
   });
 
   describe('when creating via belongsTo', function() {
-    var createdAuthor;
 
     beforeEach(function() {
       createdAuthor = article.createAuthor({ username: 'phil' });
@@ -62,6 +105,50 @@ describe('Model.hasMany + Model.belongsTo', function() {
 
     it.skip('creates hasMany collection cache', function() {
       expect(createdAuthor.articles).to.eql([article]);
+    });
+  });
+
+  describe('when hasMany collection cache is loaded', function() {
+    beforeEach(function(done) {
+      user.articleObjects.fetch().then(function() { done(); }, done);
+    });
+
+    describe('when storing existing object via belongsTo', function() {
+      beforeEach(shared.storeExistingAuthor);
+
+      it.skip('adds to hasMany collection cache', function() {
+        expect(storedAuthor.articles).to.contain(article);
+      });
+    });
+
+    describe('when storing unsaved object via belongsTo', function() {
+      beforeEach(shared.storeUnsavedAuthor);
+
+      it.skip('adds to hasMany collection cache', function() {
+        expect(storedAuthor.articles).to.contain(article);
+      });
+    });
+  });
+
+  describe('when hasMany collection cache is not loaded', function() {
+    describe('when storing existing object via belongsTo', function() {
+      beforeEach(shared.storeExistingAuthor);
+
+      it.skip('does not load hasMany collection cache', function() {
+        expect(function() {
+          user.articles;
+        }).to.throw(/articles.*not yet.*loaded/i);
+      });
+    });
+
+    describe('when storing unsaved object via belongsTo', function() {
+      beforeEach(shared.storeUnsavedAuthor);
+
+      it('does not load hasMany collection cache', function() {
+        expect(function() {
+          user.articles;
+        }).to.throw(/articles.*not yet.*loaded/i);
+      });
     });
   });
 });
