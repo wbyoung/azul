ce126d25a05ef2693d59729829d51f3199a68c0f
Whitney Young <wbyoung@fadingred.com>
2014-12-03 16:56:07 -0800

    Removed what was actually a duplicated test & moved logic to other tests.

--- test/relations/inverse_tests.js
+++ test/relations/inverse_tests.js
@@ -100,33 +100,6 @@ describe('Model.hasMany+belongsTo', function() {
     });
   });
 
-  describe('when setting via belongsTo', function() {
-    beforeEach(function() {
-      article.author = user;
-    });
-
-    it('does not load hasMany collection cache', function() {
-      expect(function() {
-        user.articles;
-      }).to.throw(/articles.*not yet.*loaded/i);
-    });
-
-    describe('when saved', function() {
-      beforeEach(function(done) {
-        article.save().then(function() { done(); }, done);
-      });
-
-      it('executes the proper sql', function() {
-        expect(adapter.executedSQL()).to.eql([
-          // note that this expectation depends on ordering of object
-          // properties which is not guaranteed to be a stable ordering.
-          ['UPDATE "articles" SET "title" = ?, "author_id" = ? ' +
-           'WHERE "id" = ?', ['Dog Psychology', 395, 828]]
-        ]);
-      });
-    });
-  });
-
   describe('when creating via hasMany', function() {
     beforeEach(function() {
       createdArticle = user.createArticle({ title: 'Hello' });
@@ -216,6 +189,17 @@ describe('Model.hasMany+belongsTo', function() {
         beforeEach(function(done) {
           storePromise.then(function() { done(); }, done);
         });
+
+        it('executes the proper sql', function() {
+          expect(adapter.executedSQL()).to.eql([
+            // included from the fetch when loading the cache
+            ['SELECT * FROM "articles" WHERE "author_id" = ?', [395]],
+            // note that this expectation depends on ordering of object
+            // properties which is not guaranteed to be a stable ordering.
+            ['UPDATE "articles" SET "title" = ?, "author_id" = ? ' +
+             'WHERE "id" = ?', ['Dog Psychology', 395, 828]]
+          ]);
+        });
       });
     });
   });
@@ -232,6 +216,15 @@ describe('Model.hasMany+belongsTo', function() {
     describe('when executed', function() {
       beforeEach(function(done) {
         storePromise.then(function() { done(); }, done);
+      });
+
+      it('executes the proper sql', function() {
+        expect(adapter.executedSQL()).to.eql([
+          // note that this expectation depends on ordering of object
+          // properties which is not guaranteed to be a stable ordering.
+          ['UPDATE "articles" SET "title" = ?, "author_id" = ? ' +
+           'WHERE "id" = ?', ['Dog Psychology', 395, 828]]
+        ]);
       });
     });
   });
