e35dd62a48d28c7d9c36ed174bed4fe1c5a55eaa
Whitney Young <wbyoung@fadingred.com>
2014-12-11 14:18:19 -0500

    Added a test for pseudo return called with no args.

---
+++ test/adapters/returning_mixin_tests.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var BluebirdPromise = require('bluebird');
+var Adapter = require('../../lib/adapters/base');
+var Database = require('../../lib/database');
+var returning = require('../../lib/adapters/mixins/returning'),
+  EmbedPseudoReturn = returning.EmbedPseudoReturn,
+  ExtractPseudoReturn = returning.ExtractPseudoReturn;
+
+var CustomAdapter,
+  db,
+  adapter;
+
+describe('Adapter with PseudoReturn', function() {
+  beforeEach(function() {
+    this.fns = {};
+    CustomAdapter = Adapter.extend({
+      _connect: BluebirdPromise.method(function() {}),
+      _disconnect: BluebirdPromise.method(function() {}),
+      _execute: BluebirdPromise.method(function() {
+        return this.fns.execute.apply(this, arguments);
+      }.bind(this))
+    });
+    CustomAdapter.reopenClass({ Phrasing: Adapter.Phrasing.extend() });
+    CustomAdapter.Phrasing.reopen(EmbedPseudoReturn);
+    CustomAdapter.reopen(ExtractPseudoReturn);
+    adapter = CustomAdapter.create({});
+    db = Database.create({ adapter: adapter });
+  });
+
+  describe('when returning is called with no arguments', function() {
+    beforeEach(function() {
+      this.fns.execute = function(connection, sql, args, returning) {
+        returning();
+        return { rows: [], fields: [] };
+      };
+    });
+
+    it('adds the an undefined value to the result', function(done) {
+      db.insert('users', { username: 'wbyoung' }).returning('identifier')
+      .then(function(result) {
+        expect(result.rows[0]).to.haveOwnProperty('identifier');
+        expect(result.rows[0].identifier).to.not.exist;
+      })
+      .then(done, done);
+    });
+  });
+
+});
