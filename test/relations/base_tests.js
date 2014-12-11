73f806e038aa2ca148e75847f3193e61382abe82
Whitney Young <wbyoung@fadingred.com>
2014-12-11 16:25:15 -0500

    Added tests for subclassing relation methods.

---
+++ test/relations/base_tests.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var Database = require('../../lib/database');
+var FakeAdapter = require('../fakes/adapter');
+var BaseRelation = require('../../lib/relations/base');
+
+require('../helpers/model');
+
+var db,
+  adapter,
+  User;
+
+describe('BaseRelation', function() {
+
+  beforeEach(function() {
+    adapter = FakeAdapter.create({});
+    db = Database.create({ adapter: adapter });
+    User = db.model('user').reopen({ username: db.attr() });
+  });
+
+  it('requires subclass to implement associate', function() {
+    var user1 = User.create();
+    var user2 = User.create();
+    var relation = BaseRelation.create('article', User);
+    expect(function() {
+      relation.associate(user1, user2);
+    }).to.throw(/associate.*must.*implemented.*subclass/i);
+  });
+
+  it('requires subclass to implement disassociate', function() {
+    var user1 = User.create();
+    var user2 = User.create();
+    var relation = BaseRelation.create('article', User);
+    expect(function() {
+      relation.disassociate(user1, user2);
+    }).to.throw(/disassociate.*must.*implemented.*subclass/i);
+  });
+
+  it('requires subclass to implement prefetch', function() {
+    var user1 = User.create();
+    var user2 = User.create();
+    var relation = BaseRelation.create('article', User);
+    expect(function() {
+      relation.prefetch(user1, user2);
+    }).to.throw(/prefetch.*must.*implemented.*subclass/i);
+  });
+});
