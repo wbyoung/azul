667058e779ac39ab0133796ae465fd5ae9c51cb8
Whitney Young <wbyoung@fadingred.com>
2015-01-08 16:16:11 -0800

    Accepting alias names.

--- test/database_tests.js
+++ test/database_tests.js
@@ -5,39 +5,49 @@ var Database = require('../lib/database');
 
 describe('Database', function() {
   it('fails with an invalid adapter', function() {
-    var connection = {
+    var config = {
       adapter: 'invalid_adapter',
-      username: 'root',
-      password: '',
-      database: 'azul_test'
+      connection: {
+        username: 'root',
+        password: '',
+        database: 'azul_test'
+      }
     };
     expect(function() {
-      Database.create(connection);
+      Database.create(config);
     }).to.throw(/no adapter.*invalid_adapter/i);
   });
 
   it('fails with an object that is not an adapter', function() {
-    var connection = {
+    var config = {
       adapter: {},
     };
     expect(function() {
-      Database.create(connection);
+      Database.create(config);
     }).to.throw(/invalid adapter/i);
   });
 
+  it('loads adapters based on alias names', function() {
+    expect(function() {
+      Database.create({ adapter: 'postgres' });
+    }).to.not.throw();
+  });
+
   it('shows require errors for non-invalid-adapter errors', function() {
-    var connection = {
+    var config = {
       adapter: '../../../test/fixtures/adapters/missing',
-      username: 'root',
-      password: '',
-      database: 'azul_test'
+      connection: {
+        username: 'root',
+        password: '',
+        database: 'azul_test'
+      }
     };
     expect(function() {
-      Database.create(connection);
+      Database.create(config);
     }).to.throw(/cannot find module.*missing/i);
   });
 
-  it('fails with when no connection is given', function() {
+  it('fails with when no configuration is given', function() {
     expect(function() {
       Database.create();
     }).to.throw(/missing connection/i);
