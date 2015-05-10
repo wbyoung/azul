ca0cbb8e93e620968f165179732eeb2c27b9131f
Whitney Young <wbyoung@fadingred.com>
2015-05-10 11:47:40 -0700

    Bound query overhaul.
     - Bound query & model query merged.
     - Bound query now inherits from select query.

--- test/query/bound_tests.js
+++ test/query/bound_tests.js
@@ -7,12 +7,13 @@ var Database = require('../../lib/database');
 var BoundQuery = require('../../lib/query/bound');
 var FakeAdapter = require('../fakes/adapter');
 var Statement = require('../../lib/grammar/statement');
+var property = require('../../lib/util/property').fn;
 
 var db,
   adapter;
 
 describe('BoundQuery', function() {
-  before(function() {
+  beforeEach(function() {
     adapter = FakeAdapter.create({});
     db = Database.create({ adapter: adapter });
   });
@@ -23,211 +24,284 @@ describe('BoundQuery', function() {
     }).to.throw(/BoundQuery must be spawned/i);
   });
 
-  it('defaults to selecting data', function() {
-    var query = db.query.bindTable('users');
-    expect(query.statement).to.eql(Statement.create(
-      'SELECT * FROM "users"', []
-    ));
-  });
-
-  it('selects data', function() {
-    var query = db.query.bindTable('users').select();
-    expect(query.statement).to.eql(Statement.create(
-      'SELECT * FROM "users"', []
-    ));
-  });
-
-  it('inserts data', function() {
-    var query = db.query.bindTable('users').insert({ name: 'Whitney' });
-    expect(query.statement).to.eql(Statement.create(
-      'INSERT INTO "users" ("name") VALUES (?)', ['Whitney']
-    ));
-  });
-
-  it('updates data', function() {
-    var query = db.query.bindTable('users').update({ name: 'Whitney' });
-    expect(query.statement).to.eql(Statement.create(
-      'UPDATE "users" SET "name" = ?', ['Whitney']
-    ));
-  });
-
-  it('deletes data', function() {
-    var query = db.query.bindTable('users').delete();
-    expect(query.statement).to.eql(Statement.create(
-      'DELETE FROM "users"', []
-    ));
-  });
-
-  it('executes raw queries', function() {
-    var query = db.query.bindTable('users').raw('SELECT * FROM "users"');
-    expect(query.statement).to.eql(Statement.create(
-      'SELECT * FROM "users"', []
-    ));
-  });
-
-  describe('pre-specified condition', function() {
-    var query;
-    before(function() {
-      query = db.query.bindTable('users').where({ name: 'Whitney' });
+  describe('when bound', function() {
+    beforeEach(function() {
+      this.query = this.boundQuery = db.query.bind(db.model('user', {
+        name: db.attr(),
+        organization: db.belongsTo(),
+      }));
     });
 
-    it('allows select', function() {
-      expect(query.select().statement).to.eql(Statement.create(
-        'SELECT * FROM "users" WHERE "name" = ?', ['Whitney']
+    it('defaults to selecting data', function() {
+      expect(this.query.statement).to.eql(Statement.create(
+        'SELECT * FROM "users"', []
       ));
     });
 
-    it('allows update', function() {
-      expect(query.update({ name: 'Whit' }).statement).to.eql(Statement.create(
-        'UPDATE "users" SET "name" = ? WHERE "name" = ?', ['Whit', 'Whitney']
+    it('selects data', function() {
+      var query = this.query.select();
+      expect(this.query.statement).to.eql(Statement.create(
+        'SELECT * FROM "users"', []
       ));
     });
 
-    it('allows delete', function() {
-      expect(query.delete().statement).to.eql(Statement.create(
-        'DELETE FROM "users" WHERE "name" = ?', ['Whitney']
+    it('inserts data', function() {
+      var query = this.query.insert({ name: 'Whitney' });
+      expect(query.statement).to.eql(Statement.create(
+        'INSERT INTO "users" ("name") VALUES (?)', ['Whitney']
       ));
     });
 
-    it('does not allow insert', function() {
-      expect(function() {
-        query.insert({ name: 'Whit' });
-      }).to.throw(/cannot.*insert.*query.*where/i);
-    });
-
-    it('does not allow raw', function() {
-      expect(function() {
-        query.raw('SELECT * FROM "users"');
-      }).to.throw(/cannot.*raw.*query.*where/i);
-    });
-  });
-
-  describe('pre-specified order', function() {
-    var query;
-    before(function() {
-      query = db.query.bindTable('users').orderBy('name');
-    });
-
-    it('does not allow insert', function() {
-      expect(function() {
-        query.insert({ name: 'Whit' });
-      }).to.throw(/cannot.*insert.*query.*orderBy/i);
-    });
-
-    it('does not allow update', function() {
-      expect(function() {
-        query.update({ name: 'Whit' });
-      }).to.throw(/cannot.*update.*query.*orderBy/i);
-    });
-
-    it('does not allow delete', function() {
-      expect(function() {
-        query.delete();
-      }).to.throw(/cannot.*delete.*query.*orderBy/i);
-    });
-
-    it('does not allow raw', function() {
-      expect(function() {
-        query.raw('SELECT * FROM "users"');
-      }).to.throw(/cannot.*raw.*query.*orderBy/i);
-    });
-  });
-
-  describe('pre-specified limit', function() {
-    var query;
-    before(function() {
-      query = db.query.bindTable('users').limit(3);
-    });
-
-    it('allows select', function() {
-      expect(query.select().statement).to.eql(Statement.create(
-        'SELECT * FROM "users" LIMIT 3', []
+    it('updates data', function() {
+      var query = this.query.update({ name: 'Whitney' });
+      expect(query.statement).to.eql(Statement.create(
+        'UPDATE "users" SET "name" = ?', ['Whitney']
       ));
     });
 
-    it('does not allow insert', function() {
-      expect(function() {
-        query.insert({ name: 'Whit' });
-      }).to.throw(/cannot.*insert.*query.*limit/i);
-    });
-
-    it('does not allow update', function() {
-      expect(function() {
-        query.update({ name: 'Whit' });
-      }).to.throw(/cannot.*update.*query.*limit/i);
-    });
-
-    it('does not allow delete', function() {
-      expect(function() {
-        query.delete();
-      }).to.throw(/cannot.*delete.*query.*limit/i);
-    });
-
-    it('does not allow raw', function() {
-      expect(function() {
-        query.raw('SELECT * FROM "users"');
-      }).to.throw(/cannot.*raw.*query.*limit/i);
-    });
-  });
-
-  describe('pre-specified join', function() {
-    var query;
-    before(function() {
-      query = db.query.bindTable('users')
-        .join('profiles', 'left', 'users.profile_id=profiles.id');
-    });
-
-    it('allows select', function() {
-      expect(query.select().statement).to.eql(Statement.create(
-        'SELECT * FROM "users" LEFT JOIN "profiles" ' +
-        'ON "users"."profile_id" = "profiles"."id"', []
+    it('deletes data', function() {
+      var query = this.query.delete();
+      expect(query.statement).to.eql(Statement.create(
+        'DELETE FROM "users"', []
       ));
     });
 
-    it('does not allow insert', function() {
-      expect(function() {
-        query.insert({ name: 'Whit' });
-      }).to.throw(/cannot.*insert.*query.*join/i);
+    it('executes raw queries', function() {
+      var query = this.query.raw('SELECT * FROM "users"');
+      expect(query.statement).to.eql(Statement.create(
+        'SELECT * FROM "users"', []
+      ));
     });
 
-    it('does not allow update', function() {
-      expect(function() {
-        query.update({ name: 'Whit' });
-      }).to.throw(/cannot.*update.*query.*join/i);
+    describe('with pre-specified condition', function() {
+      beforeEach(function() {
+        this.query = this.boundQuery.where({ name: 'Whitney' });
+      });
+
+      it('allows select', function() {
+        expect(this.query.select().statement).to.eql(Statement.create(
+          'SELECT * FROM "users" WHERE "name" = ?', ['Whitney']
+        ));
+      });
+
+      it('allows update', function() {
+        expect(this.query.update({ name: 'Whit' }).statement).to.eql(Statement.create(
+          'UPDATE "users" SET "name" = ? WHERE "name" = ?', ['Whit', 'Whitney']
+        ));
+      });
+
+      it('allows delete', function() {
+        expect(this.query.delete().statement).to.eql(Statement.create(
+          'DELETE FROM "users" WHERE "name" = ?', ['Whitney']
+        ));
+      });
+
+      it('does not allow insert', function() {
+        expect(function() {
+          this.query.insert({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*insert.*query.*where/i);
+      });
+
+      it('does not allow raw', function() {
+        expect(function() {
+          this.query.raw('SELECT * FROM "users"');
+        }.bind(this)).to.throw(/cannot.*raw.*query.*where/i);
+      });
     });
 
-    it('does not allow delete', function() {
-      expect(function() {
-        query.delete();
-      }).to.throw(/cannot.*delete.*query.*join/i);
+    describe('pre-specified order', function() {
+      beforeEach(function() {
+        this.query = this.boundQuery.orderBy('name');
+      });
+
+      it('does not allow insert', function() {
+        expect(function() {
+          this.query.insert({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*insert.*query.*orderBy/i);
+      });
+
+      it('does not allow update', function() {
+        expect(function() {
+          this.query.update({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*update.*query.*orderBy/i);
+      });
+
+      it('does not allow delete', function() {
+        expect(function() {
+          this.query.delete();
+        }.bind(this)).to.throw(/cannot.*delete.*query.*orderBy/i);
+      });
+
+      it('does not allow raw', function() {
+        expect(function() {
+          this.query.raw('SELECT * FROM "users"');
+        }.bind(this)).to.throw(/cannot.*raw.*query.*orderBy/i);
+      });
     });
 
-    it('does not allow raw', function() {
-      expect(function() {
-        query.raw('SELECT * FROM "users"');
-      }).to.throw(/cannot.*raw.*query.*join/i);
+    describe('pre-specified limit', function() {
+      beforeEach(function() {
+        this.query = this.boundQuery.limit(3);
+      });
+
+      it('allows select', function() {
+        expect(this.query.select().statement).to.eql(Statement.create(
+          'SELECT * FROM "users" LIMIT 3', []
+        ));
+      });
+
+      it('does not allow insert', function() {
+        expect(function() {
+          this.query.insert({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*insert.*query.*limit/i);
+      });
+
+      it('does not allow update', function() {
+        expect(function() {
+          this.query.update({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*update.*query.*limit/i);
+      });
+
+      it('does not allow delete', function() {
+        expect(function() {
+          this.query.delete();
+        }.bind(this)).to.throw(/cannot.*delete.*query.*limit/i);
+      });
+
+      it('does not allow raw', function() {
+        expect(function() {
+          this.query.raw('SELECT * FROM "users"');
+        }.bind(this)).to.throw(/cannot.*raw.*query.*limit/i);
+      });
     });
+
+    describe('pre-specified join', function() {
+      beforeEach(function() {
+        this.query = this.boundQuery
+          .join('profiles', 'left', 'users.profile_id=profiles.id');
+      });
+
+      it('allows select', function() {
+        expect(this.query.select().statement).to.eql(Statement.create(
+          'SELECT "users".* FROM "users" LEFT JOIN "profiles" ' +
+          'ON "users"."profile_id" = "profiles"."id"', []
+        ));
+      });
+
+      it('allows select with specific args', function() {
+        expect(this.query.select(['pk']).statement).to.eql(Statement.create(
+          'SELECT "users"."id" FROM "users" LEFT JOIN "profiles" ' +
+          'ON "users"."profile_id" = "profiles"."id"', []
+        ));
+      });
+
+      it('allows join after unbind', function() {
+        var query = this.query.unbind()
+          .join('companies', 'left', 'users.company_id=companies.id')
+          .select(['column']); // unbound query should not alter this column
+        expect(query.statement).to.eql(Statement.create(
+          'SELECT "column" FROM "users" ' +
+          'LEFT JOIN "profiles" ' +
+          'ON "users"."profile_id" = "profiles"."id" ' +
+          'LEFT JOIN "companies" ' +
+          'ON "users"."company_id" = "companies"."id"', []
+        ));
+      });
+
+      it('does not allow insert', function() {
+        expect(function() {
+          this.query.insert({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*insert.*query.*join/i);
+      });
+
+      it('does not allow update', function() {
+        expect(function() {
+          this.query.update({ name: 'Whit' });
+        }.bind(this)).to.throw(/cannot.*update.*query.*join/i);
+      });
+
+      it('does not allow delete', function() {
+        expect(function() {
+          this.query.delete();
+        }.bind(this)).to.throw(/cannot.*delete.*query.*join/i);
+      });
+
+      it('does not allow raw', function() {
+        expect(function() {
+          this.query.raw('SELECT * FROM "users"');
+        }.bind(this)).to.throw(/cannot.*raw.*query.*join/i);
+      });
+    });
+
+    it('has a fetch method', function(done) {
+      adapter.intercept(/select.*from "users"/i, {
+        fields: ['id', 'title'],
+        rows: [{ id: 1, title: '1' }]
+      });
+      this.query.fetch().then(function(rows) {
+        expect(rows).to.eql([{ id: 1, title: '1' }]);
+      })
+      .then(done, done);
+    });
+
+    it('has a fetchOne method', function(done) {
+      adapter.intercept(/select.*from "users"/i, {
+        fields: ['id', 'title'],
+        rows: [{ id: 1, title: '1' }]
+      });
+      db.select('users').fetchOne().then(function(result) {
+        expect(result).to.eql({ id: 1, title: '1' });
+      })
+      .then(done, done);
+    });
+
+    it('gives a useful error when bad attr is used in `where`', function() {
+      expect(function() {
+        this.query.where({ invalidAttr: 'value' }).all();
+      }.bind(this)).to.throw(/invalid field.*"invalidAttr".*user query.*user class/i);
+    });
+
+    it('gives a useful error when bad relation is used for `with`', function() {
+      expect(function() {
+        this.query.with('streets');
+      }.bind(this)).to.throw(/no relation.*"streets".*with.*user query/i);
+    });
+
+    it('does not allow `with` on non-select queries', function() {
+      expect(function() {
+        this.query.with('organization').update();
+      }.bind(this)).to.throw(/cannot perform.*update.*after.*with/i);
+    });
+
+    it('does not allow `with` when unbound', function() {
+      expect(function() {
+        this.query.unbind().with('organization');
+      }.bind(this)).to.throw(/cannot perform.*with.*unbound/i);
+    });
+
+    it('gives a useful error when bad relation is used for `join`', function() {
+      expect(function() {
+        this.query.join('streets');
+      }.bind(this)).to.throw(/no relation.*"streets".*join.*user query/i);
+    });
+
   });
 
-  it('has a fetch method', function(done) {
-    adapter.intercept(/select.*from "users"/i, {
-      fields: ['id', 'title'],
-      rows: [{ id: 1, title: '1' }]
-    });
-    db.query.bindTable('users').fetch().then(function(rows) {
-      expect(rows).to.eql([{ id: 1, title: '1' }]);
-    })
-    .then(done, done);
+  it('does not allow `with` on unbound queries', function() {
+    expect(function() {
+      db.select('users').with('author');
+    }).to.throw(/cannot perform.*with.*unbound/i);
   });
 
-  it('has a fetchOne method', function(done) {
-    adapter.intercept(/select.*from "users"/i, {
-      fields: ['id', 'title'],
-      rows: [{ id: 1, title: '1' }]
+  it('re-throws when auto-join is used and error is not expected', function() {
+    var Model = db.model('article', {
+      authorRelation: property(function() {
+        throw new Error('test error');
+      }),
     });
-    db.select('users').fetchOne().then(function(result) {
-      expect(result).to.eql({ id: 1, title: '1' });
-    })
-    .then(done, done);
+    expect(function() {
+      db.query.bind(Model).where({ 'author.id': 7 }).statement.sql;
+    }).to.throw(/test error/i);
   });
+
 });
