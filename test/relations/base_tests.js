9563009bddf087ff4dc6cf7c3a0ca048792bedac
Whitney Young <wbyoung@fadingred.com>
2015-05-27 13:03:33 -0700

    Always lookup relation object on model instance.
    All methods and properties that are added by a relation are now
    required to look up the relation object when they're invoked.
    Previously, the relation object used to define the method was assumed
    to be constant, but the lookup allows the relation object to be
    overridden & redefine aspects of the relation object.
    
    This is required for supporting relations in azul-transaction.

--- test/relations/base_tests.js
+++ test/relations/base_tests.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var chai = require('chai');
+var sinon = require('sinon'); chai.use(require('sinon-chai'));
 var expect = chai.expect;
 
 var Database = require('../../lib/database');
 var FakeAdapter = require('../fakes/adapter');
 var BaseRelation = require('../../lib/relations/base');
+var property = require('../../lib/util/property').fn;
 
 require('../helpers/model');
 
@@ -55,6 +57,57 @@ describe('BaseRelation', function() {
     expect(function() {
       relation.associatePrefetchResults(user1, user2);
     }).to.throw(/associatePrefetchResults.*must.*implemented.*subclass/i);
+  });
+
+  it('dispatches methods & properties to instance relation', function() {
+    var method = sinon.spy();
+    var getter = sinon.spy();
+    var setter = sinon.spy();
+
+    var Relation = BaseRelation.extend({
+      method: method,
+      get: getter,
+      set: setter
+    });
+    Relation.reopenClass({
+      methods: {
+        '<singular>Method': BaseRelation.method('method'),
+        '<singular>Property': BaseRelation.property('get', 'set'),
+      }
+    });
+
+    // this is a strategy that azul-transaction uses, so we need to support it
+    var Base = db.Model.extend({ custom: Relation.attr()() });
+    var relation = Object.create(Base.__class__.prototype.customRelation);
+    var Override = Base.extend({
+      customRelation: property(function() { return relation; })
+    });
+
+    var base = Base.create();
+    var override = Override.create();
+
+    base.customMethod();
+    override.customMethod(); // should be called with custom object
+    base.customProperty;
+    override.customProperty; // should be called with custom object
+    base.customProperty = undefined;
+    override.customProperty = undefined; // should be called with custom object
+
+    expect(method.getCall(0).thisValue)
+      .to.equal(Base.__class__.prototype.customRelation);
+    expect(method.getCall(1).thisValue)
+      .to.equal(relation);
+    expect(method).to.have.been.calledTwice;
+    expect(getter.getCall(0).thisValue)
+      .to.equal(Base.__class__.prototype.customRelation);
+    expect(getter.getCall(1).thisValue)
+      .to.equal(relation);
+    expect(getter).to.have.been.calledTwice;
+    expect(setter.getCall(0).thisValue)
+      .to.equal(Base.__class__.prototype.customRelation);
+    expect(setter.getCall(1).thisValue)
+      .to.equal(relation);
+    expect(setter).to.have.been.calledTwice;
   });
 
 });
